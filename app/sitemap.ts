4aed65ce74ed7dc84b11966c1eb28b344cdf5255
Abdul-Hafiz <abdulhafizaderemi@gmail.com>
2026-02-22 18:10:19 +0100

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -28,5 +28,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.8,
     },
+    {
+      url: `${domain}/blog/zakat-vs-zakat-al-fitr`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
 }
